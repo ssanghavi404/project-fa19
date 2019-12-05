7d29385a79683c884e515923115196ab7fc7fda9
Eric Parsonnet <e.parsonnet@berkeley.edu>
2019-12-04 20:06:33 -0500

    updated write_to_file function

--- graph_file_io.py
+++ graph_file_io.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import pickle
 
 def graph_from_input(filename):
     '''graph_from_input(str) --> nx.graph G, int source, np.ndarray[int] homes, dict locToIndex
@@ -43,8 +44,59 @@ def graph_from_input(filename):
 
         indexToLoc = {v: k for k, v in locToIndex.items()}
         return G, source, homes, indexToLoc
+    
+    
+def write_output_from_pickle(inputfile, outputfile, picklefile):
+    with open(picklefile, 'rb') as f:
+        output = pickle.load(f)
 
-def write_output_file(path, dropOffs, indexToName, filename):
+
+    G, source, homes, locToIndex = graph_from_input(inputfile)
+    invertedlocToIndex = dict()
+    for key in locToIndex:
+        invertedlocToIndex.update({locToIndex[key]:key})
+
+    #check to make sure all edges for the car path exist:
+    noderoute = []
+    for building in output['route']:
+        noderoute.append(invertedlocToIndex[building])
+    for i, node in enumerate(noderoute):
+        if i == 0:
+            continue
+        if not G.has_edge(node, noderoute[i-1]):
+            raise TypeError('G does not have edge:', node, noderoute[i-1])
+
+    write_output_file(output, outputfile)
+    return 
+    
+def write_output_file(output, filename):
+    '''output is an output dict with keys route and dropoffs
+    filename is the location + name of the .out file i.e. ./100_100.out
+    '''
+    
+    path = output['route']
+    dropOffs = output['dropoffs']
+    with open(filename, 'w') as f:
+        for step in path:
+            f.write(step + " ")
+
+        f.write("\n")
+        count = 0
+        for key in dropOffs:
+            if dropOffs[key] != []:
+                count += 1
+        f.write(str(count) + "\n")
+
+        for key in dropOffs:
+            if dropOffs[key] != []:
+                f.write(key + " ")
+                for elt in dropOffs[key]:
+                    f.write(elt + " ");
+                f.write("\n")
+                
+    return 
+
+def write_output_file_old(path, dropOffs, indexToName, filename):
     '''path is a list of integers that we follow in the car
     dropOffs is a dictionary (int -> [home, home, ...] ) mapping nodes to the homes of the TAs that get off at that node
     indexToName is a list of names corresponding to each index
@@ -67,3 +119,5 @@ def write_output_file(path, dropOffs, indexToName, filename):
                 for elt in dropOffs[key]:
                     f.write(indexToName[elt] + " ");
                 f.write("\n")
+                
+    return 
